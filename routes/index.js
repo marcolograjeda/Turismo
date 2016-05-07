f4316eb4ee5b73f869a962071cffeec0dd402438
Marco Lopez <juniorlograjeda@gmail.com>
2016-05-06 22:15:05 -0600

    Cuarto commit
    rutas

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Marco on 05/05/2016.
+ */
+var ruta=require('express').Router();
+module.exports = (function (app){
+    var departamento = require('../Controller/DepartamentoController')(app);
+
+    ruta.get('/departamento', departamento.list);
+    ruta.post('/departamento', departamento.add);
+    ruta.put('/departamento', departamento.edit);
+    ruta.delete('/departamento', departamento.delete);
+    ruta.get('/departamento/:id', departamento.sitiosDepartamento);
+
+    var usuario = require('../Controller/UsuarioController')(app);
+
+    ruta.get('/usuario', usuario.list);
+    ruta.post('/usuario', usuario.add);
+    ruta.put('/usuario', usuario.edit);
+    ruta.delete('/usuario', usuario.delete);
+
+    var comentario = require('../Controller/ComentarioController')(app);
+
+    ruta.get('/comentario', comentario.list);
+    ruta.post('/comentario', comentario.add);
+    ruta.put('/comentario', comentario.edit);
+    ruta.delete('/comentario', comentario.delete);
+    ruta.get('/comentario/:id', comentario.listarUno);
+
+    var hotel = require('../Controller/HotelController')(app);
+
+    ruta.get('/hotel', hotel.list);
+    ruta.post('/hotel', hotel.add);
+    ruta.put('/hotel', hotel.edit);
+    ruta.delete('/hotel', hotel.delete);
+    ruta.get('/hotel/:id', hotel.listarUno);
+    ruta.get('/hotel/:id', hotel.departamentoHotel);
+
+    var sitio = require('../Controller/SitioController')(app);
+
+    ruta.get('/sitio', sitio.list);
+    ruta.post('/sitio', sitio.add);
+    ruta.put('/sitio', sitio.edit);
+    ruta.delete('/sitio', sitio.delete);
+
+    var rol = require('../Controller/RolController')(app);
+
+    ruta.get('/rol', sitio.list);
+    ruta.post('/rol', sitio.add);
+    ruta.put('/rol', sitio.edit);
+    ruta.delete('/rol', sitio.delete);
+    return ruta;
+});
